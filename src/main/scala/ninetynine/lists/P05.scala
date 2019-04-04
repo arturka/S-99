67c22856c7e82183ca4ce8ccef290a46ee116faa
abdalimov.artur <artur.abdalimov@metahouse.ru>
2019-04-04 10:00:33 +0600

    add fifth exercise and tests

---
+++ src/main/scala/ninetynine/lists/P05.scala
@@ -0,0 +1,18 @@
+package ninetynine.lists
+
+import scala.annotation.tailrec
+
+object P05 {
+
+    def reverse[T](list: List[T]): List[T] = list.reverse
+
+    def reverseImpl[T](list: List[T]): List[T] = {
+        @tailrec
+        def inner(result: List[T], curList: List[T]): List[T] = curList match {
+            case Nil => result
+            case x :: xs => inner(x +: result, xs)
+        }
+
+        inner(List.empty[T], list)
+    }
+}
---
+++ src/test/scala/ninetynine/lists/P05Test.scala
@@ -0,0 +1,22 @@
+package ninetynine.lists
+
+import org.scalatest._
+
+class P05Test extends FunSuite with Matchers {
+
+    test("builtin reverse should reverse initial list") {
+        val list = List(1,2,3,4,5,6)
+        val emptyList = List.empty[Char]
+
+        P05.reverse(list) shouldBe List(6,5,4,3,2,1)
+        P05.reverse(emptyList) shouldBe List.empty[Char]
+    }
+
+    test("reverse implementation should reverse initial list") {
+        val list = List(1,2,3,4,5,6)
+        val emptyList = List.empty[Char]
+
+        P05.reverseImpl(list) shouldBe List(6,5,4,3,2,1)
+        P05.reverseImpl(emptyList) shouldBe List.empty[Char]
+    }
+}
