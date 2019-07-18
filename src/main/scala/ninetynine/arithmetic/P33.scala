69d31dd6b11e54ab4d8e4f42c6bb8ccbc2d10e77
Artur Abdalimov <aabdalimov@beeline.kz>
2019-07-18 21:39:35 +0600

    fixed typo in problem 33

--- src/main/scala/ninetynine/arithmetic/P33.scala
+++ src/main/scala/ninetynine/arithmetic/P33.scala
@@ -1,7 +1,7 @@
 package ninetynine.arithmetic
 
 trait IsCoprimeable[A] {
-    def isCoprimeTo(a: A, b: A): Boolean
+    def isCoprime(a: A, b: A): Boolean
 }
 
 object P33 {
@@ -9,6 +9,6 @@ object P33 {
     implicit val intIsCoprimable: IsCoprimeable[Int] = (a: Int, b: Int) => P32.gcd(a, b) == 1
 
     implicit class IsCoprimeableOps[A](pair: (A, A)) {
-        def isCoprimeTo(implicit ic: IsCoprimeable[A]): Boolean = ic.isCoprimeTo(pair._1, pair._2)
+        def isCoprime(implicit ic: IsCoprimeable[A]): Boolean = ic.isCoprime(pair._1, pair._2)
     }
 }
--- src/test/scala/ninetynine/arithmetic/P33Test.scala
+++ src/test/scala/ninetynine/arithmetic/P33Test.scala
@@ -5,7 +5,7 @@ import P33._
 
 class P33Test extends FunSuite with Matchers {
 
-    (3, 4).isCoprimeTo shouldBe true
-    (35, 42).isCoprimeTo shouldBe false
-    (35, 64).isCoprimeTo shouldBe true
+    (3, 4).isCoprime shouldBe true
+    (35, 42).isCoprime shouldBe false
+    (35, 64).isCoprime shouldBe true
 }
